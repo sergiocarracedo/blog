be94903a9a78f6868c388f0d51d728b83f6e95f5
Sergio Carracedo <hi@sergiocarracedo.es>
2025-06-29 18:20:41 +0200

    chore: cookies

--- cookieConsentConfig.ts
+++ cookieConsentConfig.ts
@@ -2,96 +2,12 @@ import type { CookieConsentConfig } from 'vanilla-cookieconsent';
 
 const cookiesPage = '/legal/cookie-policy';
 
-const CAT_NECESSARY = 'necessary';
-const CAT_ANALYTICS = 'analytics';
-const CAT_ADVERTISEMENT = 'advertisement';
-const CAT_FUNCTIONALITY = 'functionality';
-const CAT_SECURITY = 'security';
-
-const SERVICE_AD_STORAGE = 'ad_storage';
-const SERVICE_AD_USER_DATA = 'ad_user_data';
-const SERVICE_AD_PERSONALIZATION = 'ad_personalization';
-const SERVICE_ANALYTICS_STORAGE = 'analytics_storage';
-const SERVICE_FUNCTIONALITY_STORAGE = 'functionality_storage';
-const SERVICE_PERSONALIZATION_STORAGE = 'personalization_storage';
-const SERVICE_SECURITY_STORAGE = 'security_storage';
-
-// Define dataLayer and the gtag function.
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-
-// Set default consent to 'denied' (this should happen before changing any other dataLayer)
-gtag('consent', 'default', {
-  [SERVICE_AD_STORAGE]: 'denied',
-  [SERVICE_AD_USER_DATA]: 'denied',
-  [SERVICE_AD_PERSONALIZATION]: 'denied',
-  [SERVICE_ANALYTICS_STORAGE]: 'denied',
-  [SERVICE_FUNCTIONALITY_STORAGE]: 'denied',
-  [SERVICE_PERSONALIZATION_STORAGE]: 'denied',
-  [SERVICE_SECURITY_STORAGE]: 'denied',
-});
-
-/**
- * Update gtag consent according to the users choices made in CookieConsent UI
- */
-function updateGtagConsent() {
-  gtag('consent', 'update', {
-    [SERVICE_ANALYTICS_STORAGE]: CookieConsent.acceptedService(
-      SERVICE_ANALYTICS_STORAGE,
-      CAT_ANALYTICS
-    )
-      ? 'granted'
-      : 'denied',
-    [SERVICE_AD_STORAGE]: CookieConsent.acceptedService(SERVICE_AD_STORAGE, CAT_ADVERTISEMENT)
-      ? 'granted'
-      : 'denied',
-    [SERVICE_AD_USER_DATA]: CookieConsent.acceptedService(SERVICE_AD_USER_DATA, CAT_ADVERTISEMENT)
-      ? 'granted'
-      : 'denied',
-    [SERVICE_AD_PERSONALIZATION]: CookieConsent.acceptedService(
-      SERVICE_AD_PERSONALIZATION,
-      CAT_ADVERTISEMENT
-    )
-      ? 'granted'
-      : 'denied',
-    [SERVICE_FUNCTIONALITY_STORAGE]: CookieConsent.acceptedService(
-      SERVICE_FUNCTIONALITY_STORAGE,
-      CAT_FUNCTIONALITY
-    )
-      ? 'granted'
-      : 'denied',
-    [SERVICE_PERSONALIZATION_STORAGE]: CookieConsent.acceptedService(
-      SERVICE_PERSONALIZATION_STORAGE,
-      CAT_FUNCTIONALITY
-    )
-      ? 'granted'
-      : 'denied',
-    [SERVICE_SECURITY_STORAGE]: CookieConsent.acceptedService(
-      SERVICE_SECURITY_STORAGE,
-      CAT_SECURITY
-    )
-      ? 'granted'
-      : 'denied',
-  });
-}
-
 /**
  * Configuration for the cookie consent plugin.
  * This configuration defines the GUI options, categories of cookies,
  * and translations for the consent and preferences modals.
  */
 export const cookieConsentConfig: CookieConsentConfig = {
-  onFirstConsent: () => {
-    updateGtagConsent();
-  },
-  onConsent: () => {
-    updateGtagConsent();
-  },
-  onChange: () => {
-    updateGtagConsent();
-  },
   guiOptions: {
     consentModal: {
       layout: 'box inline',
@@ -105,61 +21,25 @@ export const cookieConsentConfig: CookieConsentConfig = {
     },
   },
   categories: {
-    [CAT_NECESSARY]: {
-      enabled: true, // this category is enabled by default
-      readOnly: true, // this category cannot be disabled
+    necessary: {
+      readOnly: true,
     },
-    [CAT_ANALYTICS]: {
-      autoClear: {
-        cookies: [
-          {
-            name: /^_ga/, // regex: match all cookies starting with '_ga'
-          },
-          {
-            name: '_gid', // string: exact cookie name
-          },
-        ],
-      },
-      // See: https://cookieconsent.orestbida.com/reference/configuration-reference.html#category-services
+    functionality: {},
+    analytics: {
       services: {
-        [SERVICE_ANALYTICS_STORAGE]: {
-          label: 'Enables storage (such as cookies) related to analytics e.g. visit duration.',
+        ga4: {
+          label:
+            '<a href="https://marketingplatform.google.com/about/analytics/terms/us/" target="_blank">Google Analytics 4</a>',
+
+          cookies: [
+            {
+              name: /^_ga/,
+            },
+          ],
         },
         disqus: {
           label: '<a href="https://disqus.com/terms-and-policies/" target="_blank">Disqus</a>',
           cookies: [{ name: /^disqus/ }, { name: /^__jid/ }, { name: /^G_ENABLED_IDPS/ }],
-        },
-      },
-    },
-    [CAT_ADVERTISEMENT]: {
-      services: {
-        [SERVICE_AD_STORAGE]: {
-          label: 'Enables storage (such as cookies) related to advertising.',
-        },
-        [SERVICE_AD_USER_DATA]: {
-          label: 'Sets consent for sending user data related to advertising to Google.',
-        },
-        [SERVICE_AD_PERSONALIZATION]: {
-          label: 'Sets consent for personalized advertising.',
-        },
-      },
-    },
-    [CAT_FUNCTIONALITY]: {
-      services: {
-        [SERVICE_FUNCTIONALITY_STORAGE]: {
-          label:
-            'Enables storage that supports the functionality of the website or app e.g. language settings.',
-        },
-        [SERVICE_PERSONALIZATION_STORAGE]: {
-          label: 'Enables storage related to personalization e.g. video recommendations.',
-        },
-      },
-    },
-    [CAT_SECURITY]: {
-      services: {
-        [SERVICE_SECURITY_STORAGE]: {
-          label:
-            'Enables storage related to security such as authentication functionality, fraud prevention, and other user protection.',
         },
       },
     },
@@ -195,7 +75,7 @@ export const cookieConsentConfig: CookieConsentConfig = {
               title: 'Strictly Necessary Cookies <span class="pm__badge">Always Enabled</span>',
               description:
                 'These cookies are essential for the website to function properly and cannot be disabled.',
-              linkedCategory: CAT_NECESSARY,
+              linkedCategory: 'necessary',
             },
             {
               title: 'Functionality Cookies',
@@ -207,49 +87,7 @@ export const cookieConsentConfig: CookieConsentConfig = {
               title: 'Analytics Cookies',
               description:
                 'These cookies help us understand how visitors interact with our website, allowing us to improve the user experience.',
-              linkedCategory: CAT_ANALYTICS,
-              cookieTable: {
-                headers: {
-                  name: 'Name',
-                  domain: 'Service',
-                  description: 'Description',
-                  expiration: 'Expiration',
-                },
-                body: [
-                  {
-                    name: '_ga',
-                    domain: 'Google Analytics',
-                    description:
-                      'Cookie set by <a href="https://business.safety.google/adscookies/">Google Analytics</a>',
-                    expiration: 'Expires after 12 days',
-                  },
-                  {
-                    name: '_gid',
-                    domain: 'Google Analytics',
-                    description:
-                      'Cookie set by <a href="https://business.safety.google/adscookies/">Google Analytics</a>',
-                    expiration: 'Session',
-                  },
-                ],
-              },
-            },
-            {
-              title: 'Advertising',
-              description:
-                'Google uses cookies for advertising, including serving and rendering ads, personalizing ads (depending on your ad settings at <a href=\"https://g.co/adsettings\">g.co/adsettings</a>), limiting the number of times an ad is shown to a user, muting ads you have chosen to stop seeing, and measuring the effectiveness of ads.',
-              linkedCategory: CAT_ADVERTISEMENT,
-            },
-            {
-              title: 'Functionality',
-              description:
-                'Cookies used for functionality allow users to interact with a service or site to access features that are fundamental to that service. Things considered fundamental to the service include preferences like the user’s choice of language, product optimizations that help maintain and improve a service, and maintaining information relating to a user’s session, such as the content of a shopping cart.',
-              linkedCategory: CAT_FUNCTIONALITY,
-            },
-            {
-              title: 'Security',
-              description:
-                'Cookies used for security authenticate users, prevent fraud, and protect users as they interact with a service.',
-              linkedCategory: CAT_SECURITY,
+              linkedCategory: 'analytics',
             },
             {
               title: 'More information',
