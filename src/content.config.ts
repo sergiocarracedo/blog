a33a8bc2af0cd606e0e16836772d3dd196b68bad
Sergio Carracedo <hi@sergiocarracedo.es>
2025-07-06 22:36:54 +0200

    post: selection on chuncked data

--- src/content.config.ts
+++ src/content.config.ts
@@ -19,6 +19,8 @@ const blog = defineCollection({
         .object({
           author: z.string().optional(),
           prompt: z.string().optional(),
+          source: z.string().optional(),
+          licence: z.string().optional(),
         })
         .optional(),
     }),
