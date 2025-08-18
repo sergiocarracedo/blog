38c37ba5410b6d9a76e9a05610fd9b38bc294fd1
Sergio Carracedo <hi@sergiocarracedo.es>
2025-08-18 23:03:46 +0200

    feat: tag slugs

--- src/utils/getTags.ts
+++ src/utils/getTags.ts
@@ -1,6 +1,7 @@
 import { getCollection } from 'astro:content';
+import { createSlug } from './createSlug';
 
-export const getTags = async (collection: string = 'blog'): Promise<string[]> => {
+export const getTags = async (collection: string = 'blog'): Promise<Map<string, string>> => {
   const posts = (await getCollection('blog')).sort(
     (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
   );
@@ -9,5 +10,9 @@ export const getTags = async (collection: string = 'blog'): Promise<string[]> =>
     (post.data.tags || []).forEach((tag) => tagSet.add(tag));
   });
 
-  return Array.from(tagSet).sort((a, b) => a.localeCompare(b));
+  return new Map<string, string>(
+    Array.from(tagSet)
+      .sort((a, b) => a.localeCompare(b))
+      .map((tag) => [createSlug(tag), tag])
+  );
 };
