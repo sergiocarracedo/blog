5e2cb37a2ef7fbd5ba503417d8fa7f80b7dee16d
Sergio Carracedo <hi@sergiocarracedo.es>
2025-11-16 23:32:08 +0100

    post: openscad llm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,27 @@
 // @ts-check
-import {rehypeHeadingIds} from '@astrojs/markdown-remark';
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import remarkDirective from 'remark-directive';
 
 import emoji from 'remark-emoji';
+import imgAttr from 'remark-imgattr';
 import './src/middleware';
-import {remarkDirectiveASCIInema} from './src/remark-plugins/remark-directive-asciinema';
-import {remarkDirectiveAstroEntryRef} from './src/remark-plugins/remark-directive-astro-entry-ref';
-import {remarkDirectiveIframe} from './src/remark-plugins/remark-directive-iframe';
-import {remarkDirectiveSpotify} from './src/remark-plugins/remark-directive-spotify';
-import {remarkDirectiveYoutube} from './src/remark-plugins/remark-directive-youtube';
+import { remarkDirectiveASCIInema } from './src/remark-plugins/remark-directive-asciinema';
+import { remarkDirectiveAstroEntryRef } from './src/remark-plugins/remark-directive-astro-entry-ref';
+import { remarkDirectiveIframe } from './src/remark-plugins/remark-directive-iframe';
+import { remarkDirectiveSpotify } from './src/remark-plugins/remark-directive-spotify';
+import { remarkDirectiveYoutube } from './src/remark-plugins/remark-directive-youtube';
 
 import icon from 'astro-icon';
 
 import pagefind from 'astro-pagefind';
 
 import cookieconsent from '@jop-software/astro-cookieconsent';
-import {cookieConsentConfig} from './cookieConsentConfig';
+import { cookieConsentConfig } from './cookieConsentConfig';
 
 import react from '@astrojs/react';
 
@@ -31,7 +32,7 @@ export default defineConfig({
   image: {
     // Enable image optimization
     domains: ['sergiocarracedo.es'],
-    remotePatterns: [{ protocol: 'https' }]
+    remotePatterns: [{ protocol: 'https' }],
   },
   markdown: {
     remarkPlugins: [
@@ -41,7 +42,8 @@ export default defineConfig({
       [remarkDirectiveSpotify, {}],
       [remarkDirectiveIframe, {}],
       [remarkDirectiveASCIInema, {}],
-      [emoji, {}]
+      [emoji, {}],
+      [imgAttr, {}],
     ],
     rehypePlugins: [
       rehypeHeadingIds,
@@ -58,13 +60,13 @@ export default defineConfig({
           },
         },
       ],
-    ]
+    ],
   },
   vite: {
     resolve: {
       alias: {
         '@': '/src',
-      }
+      },
     },
     plugins: [tailwindcss()],
   },
