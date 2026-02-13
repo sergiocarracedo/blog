a42949c00a1a4daf9c9a99994344c00877c02360
Sergio Carracedo <hi@sergiocarracedo.es>
2026-02-13 10:16:59 +0100

    post: complete

--- astro.config.mjs
+++ astro.config.mjs
@@ -47,7 +47,7 @@ export default defineConfig({
       [remarkDirectiveFloatImage, {}],
       [remarkDirectiveGallery, {}],
       [emoji, {}],
-      [imgAttr, {}],
+      [imgAttr, { extendWithLink: true }],
     ],
     rehypePlugins: [
       rehypeHeadingIds,
