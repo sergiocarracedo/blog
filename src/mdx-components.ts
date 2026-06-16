808f691b24a96670a136bfee19a666ef35a7d7e8
Sergio Carracedo <hi@sergiocarracedo.es>
2026-06-16 14:25:41 +0200

    post: skills-security

--- src/mdx-components.ts
+++ src/mdx-components.ts
@@ -1,12 +1,15 @@
 import AsciinemaPlayer from './components/AsciinemaPlayer.astro';
 import GalleryModal from './components/GalleryModal.astro';
+import GalleryWrapper from './components/GalleryWrapper';
 import SpotifyEmbed from './components/SpotifyEmbed.astro';
 import YoutubeEmbed from './components/YoutubeEmbed.astro';
 
 console.log('MDX components loaded');
+
 export const mdxComponents = {
   YoutubeEmbed,
   SpotifyEmbed,
   GalleryModal,
   AsciinemaPlayer,
+  GalleryWrapper,
 };
